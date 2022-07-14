79d7702ce3a0303f0edd32d8fadaf714eaf82367
Felipe Tsuchiya <felipetsuchiya@hotmail.com>
2022-07-14 15:09:30 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      "colorNav": "#111111",
+      "branco": "#eeeeee",
+      "preto": "#000000",
+      "laranja": "#f83f2c"
+    }
+  },
+  plugins: [],
+}
